b1ef917ae8d5396baee35bf3175e39520fd2bda4
keiseon <keiseonhamilton@gmai.com>
2019-09-15 02:06:45 -0400

    Added The completed slider Fix

--- js/main.js
+++ js/main.js
@@ -140,17 +140,19 @@ const prevSlide = () => {
   const current = document.querySelector(".current");
   const slideRightAnime = document.querySelector(".slide-right-anime");
 
-  // Remove the current class and the slide right anime
-  current.classList.remove("current");
-  slideRightAnime.classList.remove("slide-right-anime");
+  if (current.previousElementSibling) {
+    // Remove the current class and the slide right anime
+    current.classList.remove("current");
+    slideRightAnime.classList.remove("slide-right-anime");
 
-  //   Check for Previous slide
-  if (current.previousElementSibling.classList.contains("slide")) {
-    //Add the ".current" class to the Previous slide sibling
-    current.previousElementSibling.classList.add("current");
-    current.previousElementSibling.firstElementChild.classList.add(
-      "slide-right-anime"
-    );
+    //   Check for Previous slide
+    if (current.previousElementSibling.classList.contains("slide")) {
+      //Add the ".current & .slide-right-anime" classes to the Previous slide sibling
+      current.previousElementSibling.classList.add("current");
+      current.previousElementSibling.firstElementChild.classList.add(
+        "slide-right-anime"
+      );
+    }
   } else {
     // when you reach the beginning of the list of slides add the ".current" class back to the end sibling
     slides[slides.length - 1].classList.add("current");
@@ -165,28 +167,25 @@ const prevSlide = () => {
 // Button Events that call your functions
 next.addEventListener("click", e => {
   nextSlide();
+  autoSlide();
+});
+
+prev.addEventListener("click", e => {
+  prevSlide();
+  autoSlide();
+});
+
+// Setup Auto Slide Feature
+const autoSlide = () => {
   if (auto) {
     //   Clear the interval
     clearInterval(slideInterval);
     // Run the Next Slide at interval Time
     slideInterval = setInterval(nextSlide, intervalTime);
   }
-});
+};
 
-prev.addEventListener("click", e => {
-  prevSlide();
-  if (auto) {
-    //   Clear the interval
-    clearInterval(slideInterval);
-    // Run the Previous Slide at interval Time
-    slideInterval = setInterval(prevSlide, intervalTime);
-  }
-});
-
-// Setup Auto Slide Feature
-if (auto) {
-  // Run the Next Slide at interval Time
-  slideInterval = setInterval(nextSlide, intervalTime);
-}
+// Run slideshow automatically
+autoSlide();
 
 // %%%%%%%%%%%%     Slideshow Code end   %%%%%%%%%%%%%
